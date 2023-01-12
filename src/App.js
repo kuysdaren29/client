facc170091919bb04f3dba57edf157891ce043a6
kuysdaren29 <1901488@lnu.edu.ph>
2023-01-12 17:15:29 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { createTheme, ThemeProvider } from '@mui/material';
+import Home from './pages/Home';
+import JournalPage from './pages/JournalPage';
+import ModifyPage from './pages/ModifyPage';
+import Signup from './pages/Signup';
+import Login from './pages/Login';
+import { useAuthContext } from './hooks/useAuthContext';
+
+
+const theme = createTheme({
+  palette:{
+   primary:{
+    main: '#00838f'
+   },
+   secondary: {
+    main: '#24c0dc'
+   }, 
+  }
+})
+
+
 
 function App() {
+  
+  const { user } = useAuthContext()
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+  <ThemeProvider theme={theme}>
+     <BrowserRouter>
+      <Routes>
+        <Route path='/' element={user ? <Home/> : <Navigate to="/login"/>}></Route>
+        <Route path='/sign-up' element={!user ? <Signup/> : <Navigate to="/"/>}></Route>
+        <Route path='/login' element={!user ? <Login/> : <Navigate to="/"/>}></Route>
+        <Route path='/create' element={<JournalPage/>}></Route>
+        <Route path='/modify' element={<ModifyPage/>}></Route>
+      
+      </Routes>
+     </BrowserRouter>
+  </ThemeProvider>
+    
   );
 }
 
